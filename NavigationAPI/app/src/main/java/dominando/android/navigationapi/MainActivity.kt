d8d365d89c5b8b369e9a41dcb1ac47cf238c39a3
Felipe Wisniewski <felipewisniewski@gmail.com>
2019-12-10 11:49:11 -0300

    navigation api

---
+++ NavigationAPI/app/src/main/java/dominando/android/navigationapi/MainActivity.kt
@@ -0,0 +1,25 @@
+package dominando.android.navigationapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+
+    private val navControler: NavController by lazy {
+        Navigation.findNavController(this, R.id.navHostFragment)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        NavigationUI.setupActionBarWithNavController(this, navControler)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navControler.navigateUp()
+    }
+}

453639f9472f89ad137b6088b992b764351bb1e2
Felipe Wisniewski <felipewisniewski@gmail.com>
2019-12-11 11:21:39 -0300

    persistencia

--- Persistencia/app/src/main/java/dominando/android/persistencia/MainActivity.kt
+++ Persistencia/app/src/main/java/dominando/android/persistencia/MainActivity.kt
@@ -1,24 +1,24 @@
 package dominando.android.persistencia
 
 import android.content.Context
-import android.content.pm.PackageManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.text.TextUtils
 import android.util.Log
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
+import androidx.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_main.*
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.OnPermissionDenied
+import permissions.dispatcher.RuntimePermissions
 import java.io.*
 import java.lang.Exception
 import java.lang.StringBuilder
 
+@RuntimePermissions
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        val RC_STORAGE_PERMISSION = 0
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,34 +31,24 @@ class MainActivity : AppCompatActivity() {
         btnSave.setOnClickListener {
             btnSaveClick()
         }
-    }
 
-    private fun checkStoragePermission(permission: String, requestCode: Int): Boolean {
-        if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
-                Toast.makeText(this, R.string.message_permission_requested, Toast.LENGTH_SHORT).show()
-            }
-
-            ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
-            return false
+        btnOpenPref.setOnClickListener {
+            startActivity(Intent(this, ConfigActivity::class.java))
         }
-        return true
+
+        btnReadPref.setOnClickListener {
+            readPref()
+        }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
 
-        when (requestCode) {
-            RC_STORAGE_PERMISSION -> {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, R.string.permission_granted, Toast.LENGTH_SHORT).show()
-
-                } else {
-                    Toast.makeText(this, R.string.permission_denied, Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
+    @OnPermissionDenied(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    fun showDeniedForExternal() {
+        Toast.makeText(this, R.string.permission_denied, Toast.LENGTH_SHORT).show()
     }
 
     private fun btnReadClick() {
@@ -66,8 +56,8 @@ class MainActivity : AppCompatActivity() {
 
         when (type) {
             R.id.rbInternal -> loadFromInternal()
-            R.id.rbExternalPriv -> loadFromExternal(true)
-            R.id.rbExternalPublic -> loadFromExternal(false)
+            R.id.rbExternalPriv -> loadFromExternalWithPermissionCheck(true)
+            R.id.rbExternalPublic -> loadFromExternalWithPermissionCheck(false)
         }
     }
 
@@ -76,8 +66,8 @@ class MainActivity : AppCompatActivity() {
 
         when (type) {
             R.id.rbInternal -> saveToInternal()
-            R.id.rbExternalPriv -> saveToExternal(true)
-            R.id.rbExternalPublic -> saveToExternal(false)
+            R.id.rbExternalPriv -> saveToExternalWithPermissionCheck(true)
+            R.id.rbExternalPublic -> saveToExternalWithPermissionCheck(false)
         }
     }
 
@@ -101,13 +91,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun saveToExternal(privateDir: Boolean) {
-        val hasPermission = checkStoragePermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, RC_STORAGE_PERMISSION)
-
-        if (!hasPermission) {
-            return
-        }
-
+    @NeedsPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    fun saveToExternal(privateDir: Boolean) {
         val state = Environment.getExternalStorageState()
         if (state == Environment.MEDIA_MOUNTED) {
             val myDir = getExternalDir(privateDir)
@@ -133,13 +118,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun loadFromExternal(privateDir: Boolean) {
-        val hasPermission = checkStoragePermission(android.Manifest.permission.READ_EXTERNAL_STORAGE, RC_STORAGE_PERMISSION)
-
-        if (!hasPermission) {
-            return
-        }
-
+    @NeedsPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+    fun loadFromExternal(privateDir: Boolean) {
         val state = Environment.getExternalStorageState()
 
         if (state == Environment.MEDIA_MOUNTED || state == Environment.MEDIA_MOUNTED_READ_ONLY) {
@@ -198,5 +178,18 @@ class MainActivity : AppCompatActivity() {
         txtText.text = sb.toString()
     }
 
+    private fun readPref() {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
 
+        val city = prefs.getString(getString(R.string.pref_city), getString(R.string.pref_city_default))
+        val socialNetwork = prefs.getString(getString(R.string.pref_social_network), getString(R.string.pref_social_network_default))
+        val messages = prefs.getBoolean(getString(R.string.pref_messages), false)
+
+        val msg = String.format("%s = %s\n%s = %s\n%s = %s",
+            getString(R.string.title_city), city,
+            getString(R.string.title_social_network), socialNetwork,
+            getString(R.string.title_messages), messages.toString())
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
 }
